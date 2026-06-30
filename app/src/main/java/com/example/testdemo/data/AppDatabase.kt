c35a20899a63a06cea94bf5fd6533d46a52c91fd
Franklin Ho <fho@meta.com>
2026-06-30 15:38:12 -0400

    chore: tighten release and backup policy

--- app/src/main/java/com/example/testdemo/data/AppDatabase.kt
+++ app/src/main/java/com/example/testdemo/data/AppDatabase.kt
@@ -3,7 +3,7 @@ package com.example.testdemo.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [Movie::class, MovieRemoteKey::class], version = 2, exportSchema = false)
+@Database(entities = [Movie::class, MovieRemoteKey::class], version = 2, exportSchema = true)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun movieDao(): MovieDao
     abstract fun movieRemoteKeyDao(): MovieRemoteKeyDao
