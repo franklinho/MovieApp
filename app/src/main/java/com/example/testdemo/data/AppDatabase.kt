add42fef5594bc9d24583b99c715e635a1d2d41d
Franklin Ho <fho@meta.com>
2026-06-30 15:36:00 -0400

    refactor: use remote keys for movie paging

--- app/src/main/java/com/example/testdemo/data/AppDatabase.kt
+++ app/src/main/java/com/example/testdemo/data/AppDatabase.kt
@@ -3,7 +3,8 @@ package com.example.testdemo.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [Movie::class], version = 1, exportSchema = false)
+@Database(entities = [Movie::class, MovieRemoteKey::class], version = 2, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun movieDao(): MovieDao
+    abstract fun movieRemoteKeyDao(): MovieRemoteKeyDao
 }
